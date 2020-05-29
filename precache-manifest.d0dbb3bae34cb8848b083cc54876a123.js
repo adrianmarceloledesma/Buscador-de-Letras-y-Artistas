b3be217df19f2461105f0628f0821c0afda3abae
jOh <j0h@example.com>
2020-05-28 23:55:05 -0300

    Updates

---
+++ precache-manifest.d0dbb3bae34cb8848b083cc54876a123.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f305ca9689d258df55ed81ff83bbb28a",
+    "url": "/Buscador-de-Letras-y-Artistas/index.html"
+  },
+  {
+    "revision": "ca40c2ba58625a34f807",
+    "url": "/Buscador-de-Letras-y-Artistas/static/css/main.da45669d.chunk.css"
+  },
+  {
+    "revision": "40ccce050435fc14edc5",
+    "url": "/Buscador-de-Letras-y-Artistas/static/js/2.9d9c96ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Buscador-de-Letras-y-Artistas/static/js/2.9d9c96ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca40c2ba58625a34f807",
+    "url": "/Buscador-de-Letras-y-Artistas/static/js/main.ff657f42.chunk.js"
+  },
+  {
+    "revision": "b4241b09f5612fb0c6ba",
+    "url": "/Buscador-de-Letras-y-Artistas/static/js/runtime-main.7f1c9172.js"
+  },
+  {
+    "revision": "9c4eecc92c83f47d18f7b765ed995cda",
+    "url": "/Buscador-de-Letras-y-Artistas/static/media/img4.9c4eecc9.jpg"
+  }
+]);
